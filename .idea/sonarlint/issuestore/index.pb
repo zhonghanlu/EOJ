4e8f3d803ae2cd52052ca1d933a93fab1fc0d167
ZhongHanLu <77484950+zhonghanlu@users.noreply.github.com>
2024-11-01 13:27:03 +0800

    init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+U
+%src/main/java/com/emode/eoj/Main.java,1\4\14deb0175ae5736551175a8df6d48d9e12f2babe
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+c
+3src/main/java/com/emode/eoj/EmodeOJApplication.java,5\c\5c3c3a9f4b3cf9dd1fb5be792106965c8cb6e830
---
+++ src/main/java/com/emode/eoj/EmodeOJApplication.java
@@ -0,0 +1,15 @@
+package com.emode.eoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhl
+ * @create 2024/11/1 13:25
+ */
+@SpringBootApplication
+public class EmodeOJApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmodeOJApplication.class, args);
+    }
+}
